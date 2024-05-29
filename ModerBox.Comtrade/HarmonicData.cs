6549b966385e4d4aa073a0652989cdf70da3e0f0
ModerRAS <adqewsx@gmail.com>
2024-05-29 11:40:24 +0800

    完善谐波计算功能，实现导出谐波到excel的功能

---
+++ ModerBox.Comtrade/HarmonicData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModerBox.Comtrade {
+    public class HarmonicData {
+        public DateTime Time { get; set; }
+        public string Name { get; set; }
+        public int HarmonicOrder { get; set; }
+        public int Skip { get; set; }
+        public double HarmonicRms { get; set; }
+
+    }
+}
--- ModerBox.Comtrade/Util.cs
+++ ModerBox.Comtrade/Util.cs
@@ -1,5 +1,10 @@
 ﻿namespace ModerBox.Comtrade {
     public class Util {
-
+        public static IEnumerable<double[]> ChunkArray(double[] array, int chunkSize) {
+            return array
+                .Select((value, index) => new { value, index })
+                .GroupBy(x => x.index / chunkSize)
+                .Select(g => g.Select(x => x.value).ToArray());
+        }
     }
 }
