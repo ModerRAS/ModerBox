908ac8b47da4846b51a797dae4c9329eb8d11010
ModerRAS <adqewsx@gmail.com>
2024-05-30 15:55:38 +0800

    添加手动更新按钮

--- ModerBox/ViewModels/HomePageViewModel.cs
+++ ModerBox/ViewModels/HomePageViewModel.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Avalonia.Controls;
+using ModerBox.Common;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ModerBox.ViewModels {
     public class HomePageViewModel : ViewModelBase {
+        public ReactiveCommand<Unit, Unit> CheckUpdate { get; }
+        private string _log;
+        public string Log {
+            get => _log;
+            set => this.RaiseAndSetIfChanged(ref _log, value);
+        }
+        public async Task CheckUpdateTask() {
+            await Util.UpdateMyApp((s) => {
+                Log = s;
+            });
+        }
+
+        public HomePageViewModel() {
+            CheckUpdate = ReactiveCommand.CreateFromTask(CheckUpdateTask);
+        }
     }
 }
