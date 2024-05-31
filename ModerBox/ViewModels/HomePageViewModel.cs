d34556841cac7780500759da027fdd11bac4b594
ModerRAS <adqewsx@gmail.com>
2024-05-31 15:38:18 +0800

    添加备用源的更新功能

--- ModerBox/ViewModels/HomePageViewModel.cs
+++ ModerBox/ViewModels/HomePageViewModel.cs
@@ -11,19 +11,35 @@ using System.Threading.Tasks;
 namespace ModerBox.ViewModels {
     public class HomePageViewModel : ViewModelBase {
         public ReactiveCommand<Unit, Unit> CheckUpdate { get; }
+        public ReactiveCommand<Unit, Unit> CheckUpdateBackRoute { get; }
         private string _log;
         public string Log {
             get => _log;
             set => this.RaiseAndSetIfChanged(ref _log, value);
         }
         public async Task CheckUpdateTask() {
-            await Util.UpdateMyApp((s) => {
-                Log = s;
-            });
+            try {
+                await Util.UpdateMyApp((s) => {
+                    Log = s;
+                });
+            } catch (Exception ex) {
+                Log = "检查失败";
+            }
         }
 
+        public async Task CheckUpdateBackRouteTask() {
+            try {
+                await Util.UpdateMyAppBackRoute((s) => {
+                    Log = s;
+                });
+            } catch (Exception ex) {
+                Log = "检查失败";
+            }
+            
+        }
         public HomePageViewModel() {
             CheckUpdate = ReactiveCommand.CreateFromTask(CheckUpdateTask);
+            CheckUpdateBackRoute = ReactiveCommand.CreateFromTask(CheckUpdateBackRouteTask);
             _log = "点击下方按钮可以检查并更新";
         }
     }
