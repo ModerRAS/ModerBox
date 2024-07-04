bff4bcb1c423aa8cf2183d84e09b46c654ba25b7
ModerRAS <adqewsx@gmail.com>
2024-07-04 17:41:45 +0800

    添加一个判断滤波器分合闸时间的库

---
+++ ModerBox.Comtrade.FilterWaveform.Test/UnitTest1.cs
@@ -0,0 +1,8 @@
+namespace ModerBox.Comtrade.FilterWaveform.Test {
+    [TestClass]
+    public class UnitTest1 {
+        [TestMethod]
+        public void TestMethod1() {
+        }
+    }
+}
---
+++ ModerBox.Comtrade.FilterWaveform/ACFilter.cs
@@ -0,0 +1,14 @@
+﻿namespace ModerBox.Comtrade.FilterWaveform {
+    public class ACFilter {
+        public string Name { get; set; }
+        public string PhaseACurrentWave { get; set; }
+        public string PhaseBCurrentWave { get; set; }
+        public string PhaseCCurrentWave { get; set; }
+        public string PhaseASwitchClose { get; set; }
+        public string PhaseBSwitchClose { get; set; }
+        public string PhaseCSwitchClose { get; set; }
+        public string PhaseASwitchOpen { get; set; }
+        public string PhaseBSwitchOpen { get; set; }
+        public string PhaseCSwitchOpen { get; set; }
+    }
+}
