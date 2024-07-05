9bb3af45f116337df32ef98ee65d0ba3ab54f8be
ModerRAS <adqewsx@gmail.com>
2024-07-05 09:39:22 +0800

    添加导出表格用的格式类型

---
+++ ModerBox.Comtrade.FilterWaveform/ACFilterSheetSpec.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModerBox.Comtrade.FilterWaveform {
+    public class ACFilterSheetSpec {
+        public string Name { get; set; }
+        public DateTime Time { get; set; }
+        public SwitchType SwitchType { get; set; }
+        public double PhaseATimeInterval { get; set; }
+        public double PhaseBTimeInterval { get; set; }
+        public double PhaseCTimeInterval { get; set; }
+        public WorkType WorkType { get; set; }
+    }
+    public enum SwitchType {
+        Open,
+        Close
+    }
+    public enum WorkType {
+        Ok,
+        Error
+    }
+}
