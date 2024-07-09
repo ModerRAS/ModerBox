c530ea0e95989fdae11be8da150d75e537f18d63
ModerRAS <adqewsx@gmail.com>
2024-07-09 11:26:29 +0800

    优化抖动处理的参数。

--- ModerBox.Comtrade.FilterWaveform/ComtradeExtension.cs
+++ ModerBox.Comtrade.FilterWaveform/ComtradeExtension.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace ModerBox.Comtrade.FilterWaveform {
     public static class ComtradeExtension {
+        public static readonly double Jitter = 0.03;
         public static DigitalInfo GetACFilterDigital(this List<DigitalInfo> DData, string ACFilterData) {
             var matchedObjects = from a in DData
                                  where a.Name.Equals(ACFilterData)
@@ -45,12 +46,13 @@ namespace ModerBox.Comtrade.FilterWaveform {
             int zeroThreshold = 50; // 设置连续0值的样本数量阈值
             var count = 0;
             for (int i = 0; i < waveform.Length; i++) {
-                if (Math.Abs(waveform[i]) < 0.011) {
+                if (Math.Abs(waveform[i]) < Jitter) {
+                    //找确认的过零点
                     bool isZeroForThreshold = true;
 
                     // 检查接下来的样本是否连续为0
                     for (int j = i; j < i + zeroThreshold && j < waveform.Length; j++) {
-                        if (Math.Abs(waveform[j]) > 0.011) {
+                        if (Math.Abs(waveform[j]) > Jitter) {
                             count++;
                             if (count > 5) {
                                 isZeroForThreshold = false;
@@ -75,12 +77,12 @@ namespace ModerBox.Comtrade.FilterWaveform {
             int nonZeroThreshold = 50; // 设置连续非0值的样本数量阈值
 
             for (int i = 0; i < waveform.Length; i++) {
-                if (Math.Abs(waveform[i]) > 0.011) {
+                if (Math.Abs(waveform[i]) > Jitter) {
                     bool isNonZeroForThreshold = true;
                     var count = 0;
                     // 检查接下来的样本是否连续为非0,并且小于0.05
                     for (int j = i; j < i + nonZeroThreshold && j < waveform.Length; j++) {
-                        if (Math.Abs(waveform[j]) < 0.011) {
+                        if (Math.Abs(waveform[j]) < Jitter) {
                             count++;
                             if (count > 5) {
                                 isNonZeroForThreshold = false;
