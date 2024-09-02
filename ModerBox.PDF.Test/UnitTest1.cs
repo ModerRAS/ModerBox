2a22f634a17912966cd1395efa5064b573da457b
ModerRAS <adqewsx@gmail.com>
2024-09-02 16:27:45 +0800

    添加PDF的OCR相关类，未完成

--- ModerBox.PDF.Test/UnitTest1.cs
+++ ModerBox.PDF.Test/UnitTest1.cs
@@ -2,8 +2,12 @@ namespace ModerBox.PDF.Test {
     [TestClass]
     public class UnitTest1 {
         [TestMethod]
-        public void TestIsImagePDF() {
+        public void TestIsImagePDF_1() {
             Assert.IsFalse(PDFUtil.IsImagePDF("TestData/TextTableRead.pdf"));
+        }
+        [TestMethod]
+        public void TestIsImagePDF_2() {
+            Assert.IsTrue(PDFUtil.IsImagePDF("TestData/ImageTableRead.pdf"));
         }
         [TestMethod]
         public void TestReadTableFromPDF() {
