f0f5de48c340b4f351b8a9d7aa23e5313293f6a3
ModerRAS <adqewsx@gmail.com>
2024-09-22 15:57:42 +0800

    添加单元测试

--- ModerBox.Comtrade.PeriodicWork.Test/UnitTest1.cs
+++ ModerBox.Comtrade.PeriodicWork.Test/UnitTest1.cs
@@ -1,3 +1,7 @@
+using Akka.Actor;
+using ModerBox.Common;
+using ModerBox.Comtrade.PeriodicWork.Actor;
+using ModerBox.Comtrade.PeriodicWork.Protocol;
 using Newtonsoft.Json;
 
 namespace ModerBox.Comtrade.PeriodicWork.Test {
@@ -7,6 +11,60 @@ namespace ModerBox.Comtrade.PeriodicWork.Test {
         public void TestJsonDeserialization() {
             var file = File.ReadAllText("PeriodicWorkData.json");
             var data = JsonConvert.DeserializeObject<DataSpec>(file);
+        }
+
+        [TestMethod]
+        public async Task TestMonthlyWork() {
+            var actorSystem = ActorSystem.Create("PeriodicWorkActorSystem");
+            var DataSpec = JsonConvert.DeserializeObject<DataSpec>(File.ReadAllText("PeriodicWorkData.json"));
+            var MonthlyWork = actorSystem.ActorOf<PeriodicWorkActor>();
+            var result = await MonthlyWork.Ask<string>(new WorkDataProtocol() {
+                Data = DataSpec,
+                DataFilterName = "SmallSetValueZeroDriftInspection",
+                FolderPath = "testdata",
+                ExportPath = "export.xlsx"
+            });
+            Assert.AreEqual("finish", result);
+            Console.WriteLine(result);
+        }
+
+        [TestMethod]
+        public async Task TestQuarterlyWork() {
+            var actorSystem = ActorSystem.Create("PeriodicWorkActorSystem");
+            var DataSpec = JsonConvert.DeserializeObject<DataSpec>(File.ReadAllText("PeriodicWorkData.json"));
+            var MonthlyWork = actorSystem.ActorOf<PeriodicWorkActor>();
+            var result = await MonthlyWork.Ask<string>(new WorkDataProtocol() {
+                Data = DataSpec,
+                DataFilterName = "AnalogInspection",
+                FolderPath = "testdata",
+                ExportPath = "export2.xlsx"
+            });
+            Assert.AreEqual("finish", result);
+            Console.WriteLine(result);
+        }
+
+        [TestMethod]
+        public async Task TestOrthogonalData() {
+            var actorSystem = ActorSystem.Create("PeriodicWorkActorSystem");
+            var DataSpec = JsonConvert.DeserializeObject<DataSpec>(File.ReadAllText("PeriodicWorkData.json"));
+            var OrthogonalDataActor = actorSystem.ActorOf<OrthogonalDataActor>();
+            var result = await OrthogonalDataActor.Ask<OrthogonalDataReceiverProtocol>(new OrthogonalDataSenderProtocol() {
+                FolderPath = "testdata",
+                OrthogonalData = DataSpec.OrthogonalData.FirstOrDefault()
+            });
+            Console.WriteLine(result);
+        }
+
+        [TestMethod]
+        public async Task TestNonOrthogonalData() {
+            var actorSystem = ActorSystem.Create("PeriodicWorkActorSystem");
+            var DataSpec = JsonConvert.DeserializeObject<DataSpec>(File.ReadAllText("PeriodicWorkData.json"));
+            var NonOrthogonalDataActor = actorSystem.ActorOf<NonOrthogonalDataActor>();
+            var result = await NonOrthogonalDataActor.Ask<NonOrthogonalDataReceiverProtocol>(new NonOrthogonalDataSenderProtocol() {
+                FolderPath = "testdata",
+                NonOrthogonalData = DataSpec.NonOrthogonalData.FirstOrDefault()
+            });
+            Console.WriteLine(result);
         }
     }
 }
