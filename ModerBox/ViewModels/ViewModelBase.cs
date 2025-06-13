bd1bb3679b1e669c56bead2159658ba07b66a609
ModerRAS <adqewsx@gmail.com>
2025-06-13 09:55:58 +0800

    Avalonia界面翻新，改成了新的左右分栏的方式展示功能

--- ModerBox/ViewModels/ViewModelBase.cs
+++ ModerBox/ViewModels/ViewModelBase.cs
@@ -2,5 +2,17 @@
 
 namespace ModerBox.ViewModels {
     public class ViewModelBase : ReactiveObject {
+        private string? _title;
+
+        public string? Title {
+            get => _title;
+            set => this.RaiseAndSetIfChanged(ref _title, value);
+        }
+
+        private string? _icon;
+        public string? Icon {
+            get => _icon;
+            set => this.RaiseAndSetIfChanged(ref _icon, value);
+        }
     }
 }
