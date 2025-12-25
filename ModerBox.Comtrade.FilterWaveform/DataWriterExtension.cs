9a9cbade4f396a14020539be49fbdc8389516ae4
ModerRAS <adqewsx@gmail.com>
2025-12-25 09:58:56 +0800

    feat: 添加从SQLite查询结果流式写入工作表的功能，优化数据写入流程

--- ModerBox.Comtrade.FilterWaveform/DataWriterExtension.cs
+++ ModerBox.Comtrade.FilterWaveform/DataWriterExtension.cs
@@ -1,9 +1,11 @@
 ﻿using ModerBox.Common;
+using ModerBox.Comtrade.FilterWaveform.Storage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ModerBox.Comtrade.FilterWaveform {
     /// <summary>
@@ -55,6 +57,54 @@ namespace ModerBox.Comtrade.FilterWaveform {
                 };
                 }));
             dataWriter.WriteDoubleList(total, SheetName);
+        }
+
+        /// <summary>
+        /// 【流式】从 SQLite/EF Core 查询结果直接写入工作表（逐行写入，不构建大 List）。
+        /// </summary>
+        public static void WriteACFilterWaveformSwitchIntervalData(this DataWriter dataWriter, IQueryable<FilterWaveformResultEntity> query, string sheetName) {
+            var worksheet = dataWriter.Workbook.Worksheets.Add(sheetName);
+
+            var headers = new[] {
+                "名称",
+                "时间",
+                "分合闸",
+                "A相分合闸时间/ms",
+                "B相分合闸时间/ms",
+                "C相分合闸时间/ms",
+                "A相合闸电压过零点差/ms",
+                "B相合闸电压过零点差/ms",
+                "C相合闸电压过零点差/ms",
+                "A相合闸电阻投入时间/ms",
+                "B相合闸电阻投入时间/ms",
+                "C相合闸电阻投入时间/ms",
+                "波形有无异常",
+            };
+
+            for (var x = 0; x < headers.Length; x++) {
+                worksheet.Cell(1, x + 1).Value = headers[x];
+            }
+
+            var rowIndex = 2;
+            foreach (var r in query.AsNoTracking()) {
+                worksheet.Cell(rowIndex, 1).Value = r.Name;
+                worksheet.Cell(rowIndex, 2).Value = r.Time.ToString("yyyy/MM/dd HH:mm:ss.fff");
+                worksheet.Cell(rowIndex, 3).Value = r.SwitchType == SwitchType.Open ? "分闸" : "合闸";
+                worksheet.Cell(rowIndex, 4).Value = r.PhaseATimeInterval.ToString();
+                worksheet.Cell(rowIndex, 5).Value = r.PhaseBTimeInterval.ToString();
+                worksheet.Cell(rowIndex, 6).Value = r.PhaseCTimeInterval.ToString();
+
+                worksheet.Cell(rowIndex, 7).Value = r.SwitchType == SwitchType.Close ? r.PhaseAVoltageZeroCrossingDiff.ToString("F3") : "";
+                worksheet.Cell(rowIndex, 8).Value = r.SwitchType == SwitchType.Close ? r.PhaseBVoltageZeroCrossingDiff.ToString("F3") : "";
+                worksheet.Cell(rowIndex, 9).Value = r.SwitchType == SwitchType.Close ? r.PhaseCVoltageZeroCrossingDiff.ToString("F3") : "";
+
+                worksheet.Cell(rowIndex, 10).Value = r.SwitchType == SwitchType.Close && r.PhaseAClosingResistorDurationMs > 0 ? r.PhaseAClosingResistorDurationMs.ToString("F2") : "";
+                worksheet.Cell(rowIndex, 11).Value = r.SwitchType == SwitchType.Close && r.PhaseBClosingResistorDurationMs > 0 ? r.PhaseBClosingResistorDurationMs.ToString("F2") : "";
+                worksheet.Cell(rowIndex, 12).Value = r.SwitchType == SwitchType.Close && r.PhaseCClosingResistorDurationMs > 0 ? r.PhaseCClosingResistorDurationMs.ToString("F2") : "";
+
+                worksheet.Cell(rowIndex, 13).Value = r.WorkType == WorkType.Ok ? "无" : "有";
+                rowIndex++;
+            }
         }
     }
 }
