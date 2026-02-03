13957347ca639bc69e5d113b87a7ba05dd0648ca
ModerRAS <adqewsx@gmail.com>
2026-02-03 09:48:51 +0800

    fix: 修正谐波计算和接地极电流差值分析图标

--- ModerBox/ViewModels/MainWindowViewModel.cs
+++ ModerBox/ViewModels/MainWindowViewModel.cs
@@ -15,12 +15,12 @@ namespace ModerBox.ViewModels {
         public MainWindowViewModel() {
             Pages = new ObservableCollection<ViewModelBase> {
                 new HomePageViewModel { Title = "首页", Icon = "Home" },
-                new HarmonicCalculateViewModel { Title = "谐波计算", Icon = "ShowResults" },
+                new HarmonicCalculateViewModel { Title = "谐波计算", Icon = "Audio" },
                 new FilterWaveformSwitchIntervalViewModel { Title = "滤波器分合闸波形检测", Icon = "Filter" },
                 new PeriodicWorkViewModel { Title = "内置录波定期工作", Icon = "Calendar" },
-                new CurrentDifferenceAnalysisViewModel { Title = "接地极电流差值分析", Icon = "Calculator" },
-                new NewCurrentDifferenceAnalysisViewModel { Title = "接地极电流差值分析 (新版)", Icon = "Calculator" },
-                new ThreePhaseIdeeAnalysisViewModel { Title = "三相IDEE分析", Icon = "ThreeColumns" },
+                new CurrentDifferenceAnalysisViewModel { Title = "接地极电流差值分析", Icon = "Ruler" },
+                new NewCurrentDifferenceAnalysisViewModel { Title = "接地极电流差值分析 (新版)", Icon = "RulerFilled" },
+                new ThreePhaseIdeeAnalysisViewModel { Title = "三相IDEE分析", Icon = "ThreeBars" },
                 new QuestionBankConversionViewModel { Title = "题库转换", Icon = "Document" },
                 new ComtradeExportViewModel { Title = "波形通道导出", Icon = "Save" }
             };
